a87cd73a14afd8a3dc1bf51b94c3822e4addd576
Binary Wang <binarywang@gmail.com>
2016-09-28 15:29:37 +0800

    格式化日志输出

--- src/main/java/com/github/binarywang/demo/spring/handler/LogHandler.java
+++ src/main/java/com/github/binarywang/demo/spring/handler/LogHandler.java
@@ -2,10 +2,12 @@ package com.github.binarywang.demo.spring.handler;
 
 import java.util.Map;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.binarywang.demo.spring.config.WxConfig;
 
 import me.chanjar.weixin.common.session.WxSessionManager;
@@ -20,12 +22,22 @@ import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
  */
 @Component
 public class LogHandler extends AbstractHandler {
+  private static final ObjectMapper JSON = new ObjectMapper();
+  static {
+    JSON.setSerializationInclusion(Include.NON_NULL);
+    JSON.configure(SerializationFeature.INDENT_OUTPUT, Boolean.TRUE);
+  }
+
   @Override
   public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
       Map<String, Object> context, WxMpService wxMpService,
       WxSessionManager sessionManager) {
-    this.logger.info("\n接收到请求消息，内容：【{}】", ToStringBuilder
-      .reflectionToString(wxMessage, ToStringStyle.MULTI_LINE_STYLE));
+    try {
+      this.logger.info("\n接收到请求消息，内容：{}", JSON.writeValueAsString(wxMessage));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+
     return null;
   }
 
